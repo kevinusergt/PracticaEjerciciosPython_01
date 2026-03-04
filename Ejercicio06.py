91b77b0adb99a96615ac063517da0f33d66b7afb
Kevin <kvillalobos917@gmail.com>
2026-03-04 12:39:00 -0500

    cambio visual

--- Ejercicio06.py
+++ Ejercicio06.py
@@ -1,6 +1,8 @@
 
 Precio_product= float(input("Cual es el costo de su producto? \n"))
 iva_producto = Precio_product * 0.19
+
 Precio_final= iva_producto + Precio_product
+
 print("El valor iva del producto es:", iva_producto)
 print("Y el valor final del producto es de:", Precio_final )
